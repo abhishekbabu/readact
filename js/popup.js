966713157bd1dcb840916779b89e96ebd7a82891
Kevin Zhu <kzgiant09@gmail.com>
2020-10-17 16:49:37 -0700

    Add article summary tab

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var colorA = "purple", colorB = "orange";
+    var colorA = "blue", colorB = "red";
     drawScale("seq1", d3.interpolate(colorA, colorB));
 })
 
 function drawScale(id, interpolator) {
-    var data = Array.from(Array(100).keys());
+    var data = Array.from(Array(400).keys());
 
     var cScale = d3.scaleSequential()
         .interpolator(interpolator)
-        .domain([0,99]);
+        .domain([0,400]);
 
     var xScale = d3.scaleLinear()
-        .domain([0,99])
+        .domain([0,400])
         .range([0, 580]);
 
     var u = d3.select("#" + id)
