f2f539c9b4bb88475f4a6946a51921a9039b2c13
Leslie Lin <linlj@usc.edu>
2020-10-17 19:11:57 -0700

    Highlihgt

--- js/highlight.js
+++ js/highlight.js
@@ -1,15 +1,33 @@
 "use strict";
 
+let keywords = ["COVID-19", "Joe Biden", "Kamala Harris", "Donald Trump"];
+
 let elements = document.querySelectorAll(
     "h1, h2, h3, h4, h5, h6, p, li, td, caption, span, div, a, body, ul, strong, em, table, form, i, b"
-  );
+);
 
-function highlightAll(keywords) {
+function highlightAll() { // TODO: UNDO HARDCODING
     keywords.forEach(function(keyword, i) {
         highlight(keyword);
-    })
+    });
 }
 
 function highlight(keyword) {
-
+    elements.forEach(function(element, i) {
+        element.childNodes.forReach(function(child, j) {
+            if (child.nodeType === 3) {
+                if (child.textContent) {
+                    let regex = new RegExp(keyword, "i");
+                    let text = child.textContent;
+                    text = text.replace(
+                        regex,
+                        <span class="highlighted">${keyword}</span>
+                    );
+                    const newChild = document.createElement("span");
+                    newChild.innerHTML = text;
+                    element.replaceChild(newChild, child);
+                }
+            }
+        })
+    });
 }
