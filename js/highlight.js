9e2526ad294593a0e87a645deb3c1806b0031a3a
Leslie Lin <linlj@usc.edu>
2020-10-17 19:00:57 -0700

    Highlight function

--- js/highlight.js
+++ js/highlight.js
@@ -1,7 +1,15 @@
 "use strict";
 
-function highlight(keywords) {
-    keywords.forEach(function (keyword, i) {
+let elements = document.querySelectorAll(
+    "h1, h2, h3, h4, h5, h6, p, li, td, caption, span, div, a, body, ul, strong, em, table, form, i, b"
+  );
 
-    });
+function highlightAll(keywords) {
+    keywords.forEach(function(keyword, i) {
+        highlight(keyword);
+    })
+}
+
+function highlight(keyword) {
+
 }
