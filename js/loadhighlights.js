0bdf677779b44d856d0ebfd78fc387424130f010
Leslie Lin <linlj@usc.edu>
2020-10-17 18:44:38 -0700

    nvrmnd deleted all highlight progress

--- js/loadhighlights.js
+++
@@ -1,6 +0,0 @@
-// Credits to Jerome Parent-Levesque (https://github.com/jeromepl/highlighter)
-"use strict";
-
-$(document).ready(function() {
-    loadAll(window.location.hostname + window.location.pathname);
-});
