0392144caf87ff37d69f5cac9ce2d505048d3e3c
Leslie Lin <linlj@usc.edu>
2020-10-17 17:47:40 -0700

    Added highlight mechanism from the other highlight extension

---
+++ js/loadhighlights.js
@@ -0,0 +1,6 @@
+// Credits to Jerome Parent-Levesque (https://github.com/jeromepl/highlighter)
+"use strict";
+
+$(document).ready(function() {
+    loadAll(window.location.hostname + window.location.pathname);
+});
