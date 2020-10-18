80306e54b541260b426a6b908b5500a6f4bd2e90
Kevin Zhu <kzgiant09@gmail.com>
2020-10-17 18:44:31 -0700

    Add text scraping

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,51 @@
+// Loading page HTML
+var documentHTML;
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+        documentHTML = request.source;
+        var bodyHtml = /<body.*?>([\s\S]*)<\/body>/.exec(documentHTML)[1];
+        var SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+        while (SCRIPT_REGEX.test(bodyHtml)) {
+            bodyHtml = bodyHtml.replace(SCRIPT_REGEX, " ");
+        }
+        bodyHTML = bodyHtml.replace(/(?:https?|ftp):\/\/[\n\S]+/g, ' ');
+        let plainText = bodyHtml.replace(/<[^>]+>/g, ' ');
+        plainText = plainText.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'©!"#$%&()*+\-\/:;<=>?@\[\]^_`{|}~]/g, ' ');
+        plainText = plainText.replace(/\s+/g,' ').trim();
+        console.log(plainText);
+    }
+});
+
+function onWindowLoad() {
+    chrome.tabs.executeScript(null, {
+        file: "js/getPagesSource.js"
+    }, function() {
+        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+        if (chrome.runtime.lastError) {
+            console.log('There was an error injecting script : \n' + chrome.runtime.lastError.message);
+        }
+    });
+
+}
+
+window.onload = onWindowLoad;
+
+// Create the bias scale
 document.addEventListener('DOMContentLoaded', function() {
     var colorA = "blue", colorB = "red";
     drawScale("seq1", d3.interpolate(colorA, colorB));
 })
 
 function drawScale(id, interpolator) {
-    var data = Array.from(Array(400).keys());
+    var data = Array.from(Array(100).keys());
 
     var cScale = d3.scaleSequential()
         .interpolator(interpolator)
-        .domain([0,400]);
+        .domain([0,99]);
 
     var xScale = d3.scaleLinear()
-        .domain([0,400])
+        .domain([0,99])
         .range([0, 580]);
 
     var u = d3.select("#" + id)
